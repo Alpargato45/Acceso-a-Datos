e297365a15643bd3664d1764cb9bc6b9b549bdc8
Alpargato45 <jorgedelcid2004@gmail.com>
2024-01-25 10:04:56 +0100

    Ejercicios

--- Tema-4-Hibernate/Actividades/accDatJPAJueves/src/main/java/org/example/App.java
+++ Tema-4-Hibernate/Actividades/accDatJPAJueves/src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
+    static Session abrirSession() throws Exception {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        if (session == null) {
+            throw new Exception("Error abriendo la sesion");
+        }else {
+            System.out.println("Conexión Establecida");
+        }
+        return session;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try (Session miSession = abrirSession()) {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
